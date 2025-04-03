c3570ba4658b726bdcde75d34115c36696297ebf
Roshni Kumari <132980931+roshnikumari0029@users.noreply.github.com>
2025-04-03 13:16:36 +0530

    Update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -17,7 +17,8 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={' COLLECTION'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae iste reprehenderit veritatis nulla dicta
+             Our latest collection has arrived,  featuring fresh styles perfect for the season. 
+
         </p>
       </div>
       {/*Rendering Products */}
